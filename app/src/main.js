a2a5628ca8505d8cdc772b645cd723b701fc3073
Hana <momenthana@kakao.com>
2020-10-06 19:16:32 +0900

    Add electron start script

---
+++ app/src/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  if (process.env.NODE_ENV !== 'production') {
+    win.loadURL('http://localhost:3000')
+    win.webContents.openDevTools()
+  } else {
+    win.loadFile()
+  }
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
