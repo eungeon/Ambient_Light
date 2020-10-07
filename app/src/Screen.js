d8e5df0a9a566f34853a5c23a73a0ef28b5100a6
Hana <momenthana@kakao.com>
2020-10-07 20:58:28 +0900

    Add getDisplayMedia

--- app/src/Screen.js
+++ app/src/Screen.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-class Screen extends React.Component {
-  render() {
-    return (
-      <div>Screen</div>
-    );
+function Screen() {
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  if (window.require) {
+    const { desktopCapturer } = window.require('electron');
+
+    desktopCapturer.getSources({ types: ['window', 'screen'] })
+      .then(async sources => {
+        console.log(sources)
+      })
+  } else {
+    navigator.mediaDevices.getDisplayMedia({ video: true })
+      .then(stream => {
+        const video = videoRef.current
+        video.srcObject = stream;
+        const aspectRatio = stream.getVideoTracks()[0].getSettings().aspectRatio
+
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        setInterval(() => {
+          context.drawImage(video, 0, 0, aspectRatio * 30, 30);
+        }, 1000 / stream.getVideoTracks()[0].getSettings().frameRate);
+      })
   }
+
+  return (
+    <React.Fragment>
+      <video muted ref={videoRef} autoPlay />
+      <canvas ref={canvasRef} />
+    </React.Fragment>
+  );
 }
 
 export default Screen;
